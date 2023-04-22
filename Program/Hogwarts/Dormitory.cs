b800ca8e177be15893a50eb100e5f38bd513d0fb
bardia.eskandarian@gmail.com <bardia.eskandarian@gmail.com>
2023-04-22 20:28:31 +0430

    -Added Dormitory Class constructors
    -Floor property name --> FloorCount
    -Room property name --> RoomCount
    -Bed property name --> BedCount

--- Program/Hogwarts/Dormitory.cs
+++ Program/Hogwarts/Dormitory.cs
@@ -2,14 +2,29 @@
 {
     public class Dormitory
     {
+        Dormitory(HumanGender dormitoryGender,Group dormitoryGroup)
+        {
+            FloorCount = 6;
+            RoomCount = 10;
+            BedCount = 5;
+            Gender = dormitoryGender;
+            Group = dormitoryGroup;
+        }
+        Dormitory(HumanGender dormitoryGender,Group dormitoryGroup,int floorCount,int roomCount ,int bedCount)
+        {
+            FloorCount = floorCount;
+            RoomCount = roomCount;
+            BedCount = bedCount;
+            Gender = dormitoryGender;
+            Group = dormitoryGroup;
+        } 
+        
         public Group Group { get; set; }
-        //These properties I'm not sure about them yet
-        /*
-        public static int Floor { get; set; }
-        public static int Room { get; set; }
-        public static int Bed { get; set; }
+        public int FloorCount { get; set; }
+        public int RoomCount { get; set; }
+        public int BedCount { get; set; }
         public static int Code { get; set; }
-        */
+        
         public static HumanGender Gender { get; set; }
         
     }
